ebececf1a4697976c36c0d2390fa078ecdd11c0f
Augusto Ramos <walteraugusto10@hotmail.com>
2016-09-19 19:18:12 -0300

    Inclusão da header e footer para evitar repetição desnecessaria de codigo

---
+++ include/footer.php
@@ -0,0 +1,6 @@
+<!-- jQuery (obrigatório para plugins JavaScript do Bootstrap) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Inclui todos os plugins compilados (abaixo), ou inclua arquivos separadados se necessário -->
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,52 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- As 3 meta tags acima *devem* vir em primeiro lugar dentro do `head`; qualquer outro conteúdo deve vir *após* essas tags -->
-    <title>Sicade</title>
+<!--Inclusão do menu principal da index do site-->
+<?php
+  include 'include/header.php';
+?>
 
-    <!-- Bootstrap -->
-    <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim e Respond.js para suporte no IE8 de elementos HTML5 e media queries -->
-    <!-- ALERTA: Respond.js não funciona se você visualizar uma página file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <nav class="navbar navbar-inverse">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">
-        <span>SICADE</span>
-      </a>
-      </div><!--navbar-header-->
-    
-      <!--Inicio Menu-->
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-11">
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php">Home</a></li>
-                <li><a href="#">Login</a></li>
-              </ul><!--ul-->
-            </div><!--navbar-collapse-->
-          </div><!--col-md-12-->
-        </div><!--row-->
-      </div><!--container-fluid-->    
-    </nav><!--navbar-->
-    <!-- jQuery (obrigatório para plugins JavaScript do Bootstrap) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Inclui todos os plugins compilados (abaixo), ou inclua arquivos separadados se necessário -->
-    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<!--Inclusão do rodapé-->
+<?php 
+	include 'include/footer.php';
+?>
