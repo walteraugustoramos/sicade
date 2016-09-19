71b8c4b63ebd0a039455565b7884f9efc82d2782
Augusto Ramos <walteraugusto10@hotmail.com>
2016-09-19 19:26:55 -0300

    Inclusão de validação no formulario de login.

--- include/footer.php
+++ include/footer.php
@@ -2,5 +2,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Inclui todos os plugins compilados (abaixo), ou inclua arquivos separadados se necessário -->
     <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <!--Inclui os arquivos javascript do bootstrap-validator para validação de formularios-->
+    <script src="bootstrap-validator-master/js/validator.js"></script>
   </body>
 </html>
