86c5183f70e5994c00af9ce8a8b3e43ae9b36ce6
Augusto Ramos <walteraugusto10@hotmail.com>
2016-09-19 19:35:22 -0300

    Inclusão do arquivo index.php na pasta controller que ficara responsavel por controllar as requisições do usuário apos ele logar no sistema.

---
+++ controller/index.php
@@ -0,0 +1,14 @@
+<?php 
+	session_start();
+
+	// verifico se existe sessão para o usuario, se não existir redireciono para pagina de login com uma mensagem de error
+	if(!empty($_SESSION) && isset($_SESSION)){
+		// verifico a permissão do usuario e redireciono para o modulo especifico
+		if($_SESSION['user']['nivel'] == '0'){
+			header('Location:../modulo_adm/index.php');
+		}
+	}else{
+		$_SESSION['msg']['error'] = 'Faça Login';
+		header('Location:../login.php');
+	}
+ ?>
