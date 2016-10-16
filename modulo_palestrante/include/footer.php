93a1550760c234113416d4e46c188b1532a97441
Augusto Ramos <walteraugusto10@hotmail.com>
2016-10-16 21:55:53 -0200

    Inclusão de paginação de certificados no modulo palestrante.

--- modulo_palestrante/include/footer.php
+++ modulo_palestrante/include/footer.php
@@ -29,10 +29,41 @@
         });
     </script>
     
-    <!--Script de inicialização do DataTable-->
+    <!--Script de inicialização do DataTable list-eventos-->
     <script>
         $(document).ready(function() {
         $('#list-eventos').DataTable( {
+            "language": {
+                "sEmptyTable": "Nenhum registro encontrado",
+                "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+                "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+                "sInfoPostFix": "",
+                "sInfoThousands": ".",
+                "sLengthMenu": "Mostrar _MENU_ resultados por página",
+                "sLoadingRecords": "Carregando...",
+                "sProcessing": "Processando...",
+                "sZeroRecords": "Nenhum registro encontrado",
+                "sSearch": "Pesquisar",
+                "oPaginate": {
+                    "sNext": "Próximo",
+                    "sPrevious": "Anterior",
+                    "sFirst": "Primeiro",
+                    "sLast": "Último"
+                },
+                "oAria": {
+                    "sSortAscending": ": Ordenar colunas de forma ascendente",
+                    "sSortDescending": ": Ordenar colunas de forma descendente"
+                }
+            }
+        } );
+    } );
+    </script>
+
+    <!--Script de inicialização do DataTable list-certificados-->
+    <script>
+        $(document).ready(function() {
+        $('#list-certificados').DataTable( {
             "language": {
                 "sEmptyTable": "Nenhum registro encontrado",
                 "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
